18f7b99a05fd0d2bc5605dbe0b2b6b97e83ba8c6
Chun Shien <chunshien.ng@bizedge.com>
2018-06-21 16:51:32 +0800

    logout function

--- app/Feed/Feed.js
+++ app/Feed/Feed.js
@@ -40,7 +40,16 @@ export default class Feed extends Component<Props> {
 
       }
       else{
-        this.refs.navigationHelper._navigate('Login', {})
+        Alert.alert(
+            "Error",
+            "Authorization failed. Please login again.",
+            [
+              {text: 'Ok', onPress: () => {
+                this.refs.navigationHelper._navigate('Login', {})
+              }, style: 'default'},
+            ],
+            { cancelable: false }
+        )
       }
     })
   }
